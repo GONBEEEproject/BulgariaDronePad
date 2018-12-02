e012abb1e5fd8d768387d8977794200c534bcba8
GONBEEE_project_myhouse0 <gonbeee.project@gmail.com>
2018-12-03 01:02:32 +0900

    ドローンコントローラ、ブルガリアコントローラSDK導入

---
+++ Assets/My_Assets/Scripts/TelloReciever.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GON.TelloControll;
+
+public class TelloReciever : MonoBehaviour {
+
+    Controller controller = new Controller();
+
+    [SerializeField]
+    private int MoveSpeed = 1;
+
+    private void Start()
+    {
+        controller.Initialize();
+    }
+
+    public void Connect()
+    {
+        controller.Connect();
+    }
+
+    public void Takeoff()
+    {
+        controller.Takeoff();
+    }
+
+    public void Land()
+    {
+        controller.Land();
+    }
+
+    public void Down()
+    {
+        controller.Down(MoveSpeed);
+    }
+
+
+
+
+}
